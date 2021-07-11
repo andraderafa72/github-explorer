e08032bb424c8518d302e4914f95a12138efb260
Rafael Andrade <devrafael72@gmail.com>
2021-07-11 14:23:58 -0300

    Ajustes finais

--- src/Components/RepositoryList/index.tsx
+++ src/Components/RepositoryList/index.tsx
@@ -25,17 +25,17 @@ export function RepositoryList() {
     setIsLoading(true)
     const url = `https://api.github.com/users/${user}/repos`
     fetch(url)
-    .then(response => response.json())
-    .then(data =>{
-      console.log(data)
-      const repos: Repository[] = data.map((repo: { name: string; description: string; html_url: string; }) => ({
-        name: repo.name,
-        description: repo.description,
-        html_url: repo.html_url,
-      }))
-      setRepositories(repos)
-    })
-    .finally(() => {setIsLoading(false); setSearchValue('')})
+      .then(response => response.json())
+      .then(data => {
+        console.log(data)
+        const repos: Repository[] = data.map((repo: { name: string; description: string; html_url: string; }) => ({
+          name: repo.name,
+          description: repo.description,
+          html_url: repo.html_url,
+        }))
+        setRepositories(repos)
+      })
+      .finally(() => { setIsLoading(false); setSearchValue('') })
   }
 
   useEffect(() => {
@@ -44,7 +44,7 @@ export function RepositoryList() {
     const result: Repository[] = []
 
     repositories.forEach(repository => {
-      if(repository.name.search(regExp) !== -1){
+      if (repository.name.search(regExp) !== -1) {
         result.push(repository)
       }
     })
@@ -53,7 +53,7 @@ export function RepositoryList() {
   }, [searchValue])
 
   useEffect(() => {
-    if(!user) return
+    if (!user) return
     const url = `https://api.github.com/users/${user}/repos`
     fetch(url)
       .then(response => response.json())
@@ -69,20 +69,23 @@ export function RepositoryList() {
       />
 
       <h2>Lista de repositórios</h2>
-      <input type="text" value={searchValue} placeholder={`Pesquisar repositório em: ${user}`} onChange={e => setSearchValue(e.target.value)} />
+      <div className="search">
+        <input type="text" value={searchValue} placeholder={`Pesquisar repositório em: ${user}`} onChange={e => setSearchValue(e.target.value)} />
+        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M23.809 21.646l-6.205-6.205c1.167-1.605 1.857-3.579 1.857-5.711 0-5.365-4.365-9.73-9.731-9.73-5.365 0-9.73 4.365-9.73 9.73 0 5.366 4.365 9.73 9.73 9.73 2.034 0 3.923-.627 5.487-1.698l6.238 6.238 2.354-2.354zm-20.955-11.916c0-3.792 3.085-6.877 6.877-6.877s6.877 3.085 6.877 6.877-3.085 6.877-6.877 6.877c-3.793 0-6.877-3.085-6.877-6.877z" /></svg>
+      </div>
 
-      { isLoading ? (
+      {isLoading ? (
         <div className="loading-wrapper">
           <div className="loading"></div>
         </div>
       ) : (
-      <ul>
-        {!searchResult[0] ? repositories.map(repository => (
-          <RepositoryItem repository={repository} key={repository.name} />
-        )) : searchResult.map(repository => (
-          <RepositoryItem repository={repository} key={repository.name} />
-        ))}
-      </ul>
+        <ul>
+          {!searchResult[0] ? repositories.map(repository => (
+            <RepositoryItem repository={repository} key={repository.name} />
+          )) : searchResult.map(repository => (
+            <RepositoryItem repository={repository} key={repository.name} />
+          ))}
+        </ul>
       )}
     </section>
   )
